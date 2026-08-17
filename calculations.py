f1b2c3b7eee6a78cd4333ed8d69984c4330984cd
Admin <admin@Umulqayr-iMac.local>
2026-08-17 11:07:04 +0100

    Updated calculations

--- calculations.py
+++ calculations.py
@@ -1,7 +1,7 @@
 import math
 
 
-# 1. Calculate transformer impedance
+# 1. Calculate transformer impedance:
 def calculate_transformer_impedance(impedance_percentage, voltage, rating):
     impedance = (
         impedance_percentage / 100
@@ -10,14 +10,14 @@ def calculate_transformer_impedance(impedance_percentage, voltage, rating):
     return impedance
 
 
-# 2. Calculate cable impedance
+# 2. Calculate cable impedance:
 def calculate_cable_impedance(impedance_per_km, cable_length):
     impedance = impedance_per_km * cable_length
 
     return impedance
 
 
-# 3. Calculate three-phase fault current
+# 3. Calculate three-phase fault current:
 def calculate_fault_current(voltage, total_impedance):
     fault_current = voltage / (
         math.sqrt(3) * total_impedance
@@ -26,7 +26,7 @@ def calculate_fault_current(voltage, total_impedance):
     return fault_current
 
 
-# 4. Calculate fault level
+# 4. Calculate fault level:
 def calculate_fault_level(voltage, fault_current):
     fault_level = (
         math.sqrt(3) * voltage * fault_current
@@ -42,18 +42,5 @@ def calculate_voltage_drop(current, cable_impedance):
     return voltage_drop
 
 
-transformer_z = calculate_transformer_impedance(
-    6,
-    11000,
-    2000000
-)
 
-cable_z = calculate_cable_impedance(
-    0.1,
-    2
-)
-
-total_z = transformer_z + cable_z
-
-print(total_z)
 
